43080d1ad4338c8bf0ba65695be26903b4f37ddf
Faris Al-khatahtbeh <48871391+farisalkhat@users.noreply.github.com>
2021-03-08 19:11:23 -0500

    Removed server files in preparation to deploy front end and back end separately.

--- server/models/counters.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
-const rmSchema = new Schema({
-    name:String,
-    sequence_value:Number
-})
-
-module.exports = mongoose.model('counter',rmSchema,'counters')
